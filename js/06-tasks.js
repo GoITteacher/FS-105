499a93ea83f2ac387edfd712f9f73401561e7666
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-04-10 19:18:07 +0300

    update

--- js/06-tasks.js
+++ js/06-tasks.js
@@ -30,3 +30,53 @@ const isDnd = true;
  */
 
 const name = 'Vasya';
+
+// ================================
+
+console.log(true && 3);
+console.log(false && 3);
+console.log(true && 4 && 'kiwi');
+console.log(true && 0 && 'kiwi');
+
+console.log(true || 3);
+console.log(true || 3 || 4);
+console.log(true || false || 7);
+console.log(null || 2 || undefined);
+
+console.log((1 && null && 2) > 0);
+console.log(null || (2 && 3) || 4);
+
+// ===================================
+
+console.log(1 && 5);
+console.log(5 && 1);
+console.log(0 && 2);
+console.log(2 && 0);
+console.log('' && 'Mango');
+console.log('Mango' && '');
+console.log('Mango' && 'Poly');
+console.log('Poly' && 'Mango');
+
+// ===================================
+
+console.log(true || false);
+console.log(false || true);
+console.log(true || true);
+
+console.log(3 || false);
+console.log(false || 3);
+console.log(3 || true);
+console.log(true || 3);
+
+// ===================================
+
+console.log(!true);
+console.log(!false);
+console.log(!3);
+console.log(!'Mango');
+console.log(!0);
+console.log(!'');
+
+// const isOnline = true;
+// const isNotOnline = !isOnline;
+// ===================================
