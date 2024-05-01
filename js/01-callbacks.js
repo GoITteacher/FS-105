93cbd98e15343ea4b93fd8637827ba023def345f
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-05-01 20:43:44 +0300

    update

--- js/01-callbacks.js
+++ js/01-callbacks.js
@@ -6,15 +6,85 @@
  * - Інлайн-колбеки
  */
 
+//!============================================================
+// function test(x) {
+// const x = foo;
+//   x();
+// }
+
+// function foo() {
+//   console.log('Hello foo');
+// }
+
+// test(foo);
+//!============================================================
+
+// function myDay(test) {
+//   console.log('застели ліжко');
+//   console.log('почистити зуби');
+//   console.log('поснідати');
+//   console.log('зібратись у школу');
+//   console.log('піти до школи');
+//   console.log('позайматись');
+//   console.log('повернутись зі школи');
+
+//   callback();
+
+//   console.log('погуляти');
+//   console.log('зробити дз');
+//   console.log('спати\n\n\n');
+// }
+
+// function goToMusicSchool() {
+//   console.log('піти до муз школи');
+//   console.log('повернутись муз школи');
+// }
+
+// function goToDanceSchool() {
+//   console.log('піти до школи танців');
+//   console.log('повернутись зі школи танців');
+// }
+
+// myDay(goToMusicSchool);
+// myDay(goToDanceSchool);
+// myDay(goToDanceSchool);
+
+//!============================================================
 /**
  * Функція calc(a, b, callback)
  */
 
-function calc(a, b) {}
+// function calc(a, b, myOperation) {
+//   console.log(`A: ${a}, B: ${b}`);
+//   const result = myOperation(a, b);
+//   console.log(`Result: ${result}`);
+// }
 
-calc(2, 3);
+// function add(x1, x2) {
+//   return x1 + x2;
+// }
 
-calc(10, 8);
+// function sub(x1, x2) {
+//   return x1 - x2;
+// }
+
+// function mult(x1, x2) {
+//   return x1 * x2;
+// }
+
+// function div(x1, x2) {
+//   return x1 / x2;
+// }
+
+// function area(x1, x2) {
+//   return x1 * x2 * Math.PI;
+// }
+
+// function toString(x1, x2) {
+//   return `${x1}-${x2}`;
+// }
+
+// calc(2, 3, toString);
 
 /**
  * Напишіть функцію each(array, callback), яка першим параметром очікує масив,
@@ -23,18 +93,29 @@ calc(10, 8);
  * результати виклику коллбека.
  */
 
-function each(array, callback) {}
+function each(array, callback) {
+  const result = [];
+  for (const item of array) {
+    const myNum = callback(item);
+    result.push(myNum);
+  }
+  return result;
+}
+
+//!===========================================================
 
 // console.log(
-//   each([64, 49, 36, 25, 16], function (value) {
+//   each([1, 4, 6, 8], function (value) {
 //     return value * 2;
-//   })
+//   }),
 // );
+
 // console.log(
-//   each([64, 49, 36, 25, 16], function (value) {
-//     return value - 10;
-//   })
+//   each(['Hello', 'world', 'test'], function (str) {
+//     return str.toUpperCase();
+//   }),
 // );
+
 // console.log(
 //   each([64, 49, 36, 25, 16], function (value) {
 //     return Math.sqrt(value);
@@ -50,3 +131,20 @@ function each(array, callback) {}
 //     return Math.floor(value);
 //   })
 // );
+
+// ========================================
+// const arr1 = [1, 2, 3, 45, 6, 7, 8, 8];
+// const arr2 = [1, 2, 3, 45, 6, 7, 8, 8];
+// const arr3 = [1, 2, 3, 45, 6, 7, 8, 8];
+
+// function callback1(item) {
+//   return item / 4;
+// }
+
+// const result1 = each(arr1, callback1);
+// const result2 = each(arr2, callback1);
+// const result3 = each(arr3, callback1);
+
+// console.log(result1);
+// console.log(result2);
+// console.log(result3);
--- js/02-arrow-fns.js
+++ js/02-arrow-fns.js
@@ -6,43 +6,69 @@
  * - Інлайн стрілочні функції
  */
 
-function add(a, b, c) {
-  return a + b + c;
-}
+//!===============================================================
 
-// const addArrow =
+// const sayHello = (firstname, lastname) => {
+//   console.log(`Hello ${firstname} ${lastname}`);
+// };
 
-console.log(add(5, 10, 15));
+// sayHello('Test1', 'Test2');
+// sayHello(10, 20);
+
+//!===============================================================
+// function add(a, b, c) {
+//   return a + b + c;
+// }
+
+// const addArrow = (a, b, c) => a + b + c;
+
+// console.log(add(5, 10, 15));
 // console.log(addArrow(5, 10, 15));
 
+// const changeStr = (str1, str2) => (str1 + str2).toUpperCase().slice(1, -1);
+
+// console.log(changeStr('Hello', 'World'));
+//!===============================================================
+
+// const foo = (...args) => {
+//   console.log(args);
+// };
+
+// foo(1, 2, 3, 4, 5, 1, 2);
+
+//!===============================================================
+
+// const changeStr = str => str.toUpperCase().slice(1, -1);
+
+// console.log(changeStr('Hello world'));
+
+//!===============================================================
 /**
  * ---------------------------
  */
-function fnA() {
-  return {
-    a: 5,
-  };
-}
+// function fnA() {
+//   return {
+//     a: 5,
+//   };
+// }
 
-console.log(fnA());
+// console.log(fnA());
 
-// const arrowFnA =
+// const arrowFnA = uservalue => ({
+//   x: uservalue,
+// });
 
-// console.log(arrowFnA());
+// console.log(arrowFnA(10));
+// console.log(arrowFnA('Hello'));
 
 /**
  * Функція calc(a, b, callback)
  */
 
-function calc(a, b, callback) {
-  const result = callback(a, b);
-  console.log(result);
-}
+// function calc(a, b, callback) {
+//   const result = callback(a, b);
+//   console.log(result);
+// }
 
-calc(2, 3, function (x, y) {
-  return x + y;
-});
-
-calc(10, 8, function (x, y) {
-  return x - y;
-});
+// calc(2, 3, (x, y) => x + y);
+// calc(10, 8, (x, y) => x - y);
