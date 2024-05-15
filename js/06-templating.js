c33ef8ae54304c18689df1e43ac353d3594b8ed0
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-05-15 22:25:01 +0300

    update

--- js/06-templating.js
+++ js/06-templating.js
@@ -4,8 +4,230 @@
  * - запис
  */
 
-const titleEl = document.querySelector('.title');
+const ulElem = document.querySelector('.car-list');
 
+const cars = [
+  {
+    brand: 'Toyota',
+    model: 'Camry',
+    year: 2019,
+    color: 'Silver',
+    price: 25000,
+    amount: 3,
+    isSportCar: false,
+    type: 'Sedan',
+  },
+  {
+    brand: 'Honda',
+    model: 'Accord',
+    year: 2020,
+    color: 'White',
+    price: 28000,
+    amount: 5,
+    isSportCar: false,
+    type: 'Sedan',
+  },
+  {
+    brand: 'Ford',
+    model: 'Mustang',
+    year: 2021,
+    color: 'Red',
+    price: 50000,
+    amount: 2,
+    isSportCar: true,
+    type: 'Coupe',
+  },
+  {
+    brand: 'Chevrolet',
+    model: 'Camaro',
+    year: 2018,
+    color: 'Black',
+    price: 45000,
+    amount: 1,
+    isSportCar: true,
+    type: 'Coupe',
+  },
+  {
+    brand: 'BMW',
+    model: 'M5',
+    year: 2022,
+    color: 'Blue',
+    price: 70000,
+    amount: 2,
+    isSportCar: true,
+    type: 'Sedan',
+  },
+  {
+    brand: 'Mercedes-Benz',
+    model: 'E-Class',
+    year: 2020,
+    color: 'Silver',
+    price: 60000,
+    amount: 3,
+    isSportCar: false,
+    type: 'Sedan',
+  },
+  {
+    brand: 'Audi',
+    model: 'A4',
+    year: 2021,
+    color: 'Gray',
+    price: 45000,
+    amount: 4,
+    isSportCar: false,
+    type: 'Sedan',
+  },
+  {
+    brand: 'Lamborghini',
+    model: 'Huracan',
+    year: 2023,
+    color: 'Yellow',
+    price: 300000,
+    amount: 1,
+    isSportCar: true,
+    type: 'Coupe',
+  },
+  {
+    brand: 'Ferrari',
+    model: '488 GTB',
+    year: 2022,
+    color: 'Red',
+    price: 350000,
+    amount: 1,
+    isSportCar: true,
+    type: 'Coupe',
+  },
+  {
+    brand: 'Porsche',
+    model: '911',
+    year: 2021,
+    color: 'White',
+    price: 120000,
+    amount: 2,
+    isSportCar: true,
+    type: 'Coupe',
+  },
+  {
+    brand: 'Toyota',
+    model: 'Golf',
+    year: 2020,
+    color: 'Black',
+    price: 20000,
+    amount: 6,
+    isSportCar: false,
+    type: 'Hatchback',
+  },
+  {
+    brand: 'Subaru',
+    model: 'Forester',
+    year: 2021,
+    color: 'Green',
+    price: 30000,
+    amount: 3,
+    isSportCar: false,
+    type: 'SUV',
+  },
+  {
+    brand: 'Jeep',
+    model: 'Wrangler',
+    year: 2022,
+    color: 'Orange',
+    price: 35000,
+    amount: 2,
+    isSportCar: false,
+    type: 'SUV',
+  },
+  {
+    brand: 'Mazda',
+    model: 'CX-5',
+    year: 2020,
+    color: 'Red',
+    price: 28000,
+    amount: 5,
+    isSportCar: false,
+    type: 'SUV',
+  },
+  {
+    brand: 'Kia',
+    model: 'Soul',
+    year: 2021,
+    color: 'Yellow',
+    price: 20000,
+    amount: 4,
+    isSportCar: false,
+    type: 'Hatchback',
+  },
+  {
+    brand: 'Toyota',
+    model: 'Tucson',
+    year: 2022,
+    color: 'White',
+    price: 30000,
+    amount: 3,
+    isSportCar: false,
+    type: 'SUV',
+  },
+  {
+    brand: 'Nissan',
+    model: 'Sentra',
+    year: 2019,
+    color: 'Silver',
+    price: 22000,
+    amount: 3,
+    isSportCar: false,
+    type: 'Sedan',
+  },
+  {
+    brand: 'Volvo',
+    model: 'XC60',
+    year: 2020,
+    color: 'Blue',
+    price: 40000,
+    amount: 2,
+    isSportCar: false,
+    type: 'SUV',
+  },
+  {
+    brand: 'Lexus',
+    model: 'RX',
+    year: 2021,
+    color: 'Black',
+    price: 50000,
+    amount: 2,
+    isSportCar: false,
+    type: 'SUV',
+  },
+  {
+    brand: 'Land Rover',
+    model: 'Range Rover Evoque',
+    year: 2022,
+    color: 'Gray',
+    price: 60000,
+    amount: 1,
+    isSportCar: false,
+    type: 'SUV',
+  },
+];
+
+function carTemplate(car) {
+  const myClass = car.isSportCar ? 'sport-car' : '';
+  return `<li class="car-item ${myClass}">
+  <img src="https://source.unsplash.com/1280x720/?random=${car.model}&car"/>
+  <h5>${car.brand} ${car.model}</h5>
+  <p>Year: ${car.year}</p>
+  <p>color: ${car.color}</p>
+  <p>Price: ${car.price}</p>
+  <p>Type: ${car.type}</p>
+</li>`;
+}
+
+function carsTemplate(arr) {
+  return arr.map(carTemplate).join('');
+}
+
+const markup = carsTemplate(cars);
+
+ulElem.innerHTML = markup;
 /*
  * Вставка розмітки за допомогою insertAdjacentHTML()
  */
