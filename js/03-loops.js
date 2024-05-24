f8d5e8f48034d8f43bfd42b48112b12b9026b914
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-05-24 21:25:40 +0300

    update

--- js/03-loops.js
+++ js/03-loops.js
@@ -2,29 +2,266 @@
  * Деструктуризація об'єкта в циклі
  */
 
-const users = [
+const cars = [
   {
-    name: "Kate",
+    brand: 'Toyota',
+    model: 'Camry',
+    year: 2019,
+    color: 'Silver',
+    price: 25000,
+    amount: 3,
+    isSportCar: false,
+    type: 'Sedan',
   },
   {
-    name: "Alex",
+    brand: 'Honda',
+    model: 'Accord',
+    year: 2020,
+    color: 'White',
+    price: 28000,
+    amount: 5,
+    isSportCar: false,
+    type: 'Sedan',
   },
   {
-    name: "Mark",
+    brand: 'Ford',
+    model: 'Mustang',
+    year: 2021,
+    color: 'Red',
+    price: 50000,
+    amount: 2,
+    isSportCar: true,
+    type: 'Coupe',
+  },
+  {
+    brand: 'Chevrolet',
+    model: 'Camaro',
+    year: 2018,
+    color: 'Black',
+    price: 45000,
+    amount: 1,
+    isSportCar: true,
+    type: 'Coupe',
+  },
+  {
+    brand: 'BMW',
+    model: 'M5',
+    year: 2022,
+    color: 'Blue',
+    price: 70000,
+    amount: 2,
+    isSportCar: true,
+    type: 'Sedan',
+  },
+  {
+    brand: 'Mercedes-Benz',
+    model: 'E-Class',
+    year: 2020,
+    color: 'Silver',
+    price: 60000,
+    amount: 3,
+    isSportCar: false,
+    type: 'Sedan',
+  },
+  {
+    brand: 'Audi',
+    model: 'A4',
+    year: 2021,
+    color: 'Gray',
+    price: 45000,
+    amount: 4,
+    isSportCar: false,
+    type: 'Sedan',
+  },
+  {
+    brand: 'Lamborghini',
+    model: 'Huracan',
+    year: 2023,
+    color: 'Yellow',
+    price: 300000,
+    amount: 1,
+    isSportCar: true,
+    type: 'Coupe',
+  },
+  {
+    brand: 'Ferrari',
+    model: '488 GTB',
+    year: 2022,
+    color: 'Red',
+    price: 350000,
+    amount: 1,
+    isSportCar: true,
+    type: 'Coupe',
+  },
+  {
+    brand: 'Porsche',
+    model: '911',
+    year: 2021,
+    color: 'White',
+    price: 120000,
+    amount: 2,
+    isSportCar: true,
+    type: 'Coupe',
+  },
+  {
+    brand: 'Toyota',
+    model: 'Golf',
+    year: 2020,
+    color: 'Black',
+    price: 20000,
+    amount: 6,
+    isSportCar: false,
+    type: 'Hatchback',
+  },
+  {
+    brand: 'Subaru',
+    model: 'Forester',
+    year: 2021,
+    color: 'Green',
+    price: 30000,
+    amount: 3,
+    isSportCar: false,
+    type: 'SUV',
+  },
+  {
+    brand: 'Jeep',
+    model: 'Wrangler',
+    year: 2022,
+    color: 'Orange',
+    price: 35000,
+    amount: 2,
+    isSportCar: false,
+    type: 'SUV',
+  },
+  {
+    brand: 'Mazda',
+    model: 'CX-5',
+    year: 2020,
+    color: 'Red',
+    price: 28000,
+    amount: 5,
+    isSportCar: false,
+    type: 'SUV',
+  },
+  {
+    brand: 'Kia',
+    model: 'Soul',
+    year: 2021,
+    color: 'Yellow',
+    price: 20000,
+    amount: 4,
+    isSportCar: false,
+    type: 'Hatchback',
+  },
+  {
+    brand: 'Toyota',
+    model: 'Tucson',
+    year: 2022,
+    color: 'White',
+    price: 30000,
+    amount: 3,
+    isSportCar: false,
+    type: 'SUV',
+  },
+  {
+    brand: 'Nissan',
+    model: 'Sentra',
+    year: 2019,
+    color: 'Silver',
+    price: 22000,
+    amount: 3,
+    isSportCar: false,
+    type: 'Sedan',
+  },
+  {
+    brand: 'Volvo',
+    model: 'XC60',
+    year: 2020,
+    color: 'Blue',
+    price: 40000,
+    amount: 2,
+    isSportCar: false,
+    type: 'SUV',
+  },
+  {
+    brand: 'Lexus',
+    model: 'RX',
+    year: 2021,
+    color: 'Black',
+    price: 50000,
+    amount: 2,
+    isSportCar: false,
+    type: 'SUV',
+  },
+  {
+    brand: 'Land Rover',
+    model: 'Range Rover Evoque',
+    year: 2022,
+    color: 'Gray',
+    price: 60000,
+    amount: 1,
+    isSportCar: false,
+    type: 'SUV',
   },
 ];
 
+//!===============================================================
 /**
  * Без деструктуризації
  */
-const names = [];
-
-for (const user of users) {
-  names.push(user.name);
-}
-
-console.log(names);
+// for (const car of cars) {
+//   console.log(car.brand, car.model);
+// }
 
 /**
  * З деструктуризацією
  */
+
+// for (const { brand, model } of cars) {
+//   console.log(brand, model);
+// }
+
+//!===============================================================
+
+const car = {
+  brand: 'Toyota',
+  model: 'Camry',
+  year: 2019,
+  color: 'Silver',
+  price: 25000,
+  amount: 3,
+  isSportCar: false,
+  type: 'Sedan',
+};
+
+// const arr = Object.entries(car);
+
+// for (const [key, value] of arr) {
+//   console.log(key, value);
+// }
+
+// const colors = [
+//   [123, 23, 12],
+//   [31, 21, 42],
+//   [13, 3, 2],
+//   [2, 12, 12],
+//   [34, 23, 12],
+//   [45, 23, 12],
+//   [12, 23, 65],
+//   [45, 56, 12],
+//   [123, 23, 12],
+// ];
+
+// for (const [red, green, blue] of colors) {
+//   console.log(`Red:${red}; green:${green}; blue:${blue}`);
+// }
+
+//!===============================================================
+
+// cars.map(({ brand, model }) => {
+//   return `
+//     <li>${brand} ${model}</li>
+//   `;
+// });
+
+// cars.filter(({ isSportCar }) => isSportCar);
